ac8f181b59844a411acdae00a2f5f29e5c34cf20
HarryMirdin <harry@mirdin.com>
2025-07-24 16:46:51 +0800

    Out-of-scope files removed

--- contracts/launchpad/interfaces/IUniV2Factory.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.27;
-
-interface IUniV2Factory {
-    function createPair(address tokenA, address tokenB) external returns (address pair);
-}
--- contracts/launchpad/interfaces/IUniswapV2FactoryMinimal.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.8.27;
-
-interface IUniswapV2FactoryMinimal {
-    function createPair(address tokenA, address tokenB) external returns (address pair);
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
-}
